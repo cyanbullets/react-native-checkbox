b79099ff58179ea49c4caa00366ddc4623e403f1
cyanbullets <86730474+cyanbullets@users.noreply.github.com>
2023-07-23 16:21:04 -0500

    exports the ios and android checkbox into one checkbox

--- src/js/CheckBox.ts
+++ src/js/CheckBox.ts
@@ -1,1 +1,7 @@
-export * from './CheckBox';
+import { Platform } from 'react-native';
+import IOSCheckBox from './CheckBox.ios';
+import AndroidCheckBox from './CheckBox.android';
+
+const CheckBox = Platform.OS === 'ios' ? IOSCheckBox : AndroidCheckBox;
+
+export default CheckBox;
